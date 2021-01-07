264178ddff21d89aff8ccaf290bf55e6152aa8da
semo4 <fotboy1788@hotmail.com>
2021-01-07 23:15:27 +0200

    add javascript code

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 'use strict';
 
 var rmvBtn = document.getElementById('list-one');
-var label = document.querySelector("label"); 
-rmvBtn.addEventListener('click', function(){
-   var rmvCheckBoxes = document.getElementsByName('check');
 
-    for(var i = 0; i < rmvCheckBoxes.length; i++)
-    {
-        if(rmvCheckBoxes[i].checked)
-        {
-            //console.log(rmvCheckBoxes[i].value);
+rmvBtn.addEventListener('click', function () {
+    var rmvCheckBoxes = document.querySelectorAll('check');
+
+    for (var i = 0; i < rmvCheckBoxes.length; i++) {
+        if (rmvCheckBoxes[i].value == "on") {
+            var label = document.querySelector("label");
+            console.log(rmvCheckBoxes[i].value);
             //console.log(label.textContent);
-
             // removeElm(rmvCheckBoxes[i]); 
-            
-            // label.setAttribute("for", rmvCheckBoxes[i].id)
-            var del = document.createElement("del");
-            del.textContent = rmvCheckBoxes[i].value;
-            console.log("vlauaa" + del.textContent);
-            label.textContent.replace(label.textContent, del.textContent );
-            label.appendChild(del);
-
+            label.setAttribute("for", rmvCheckBoxes[i].id);
+            // var del = document.createElement("del");
+            //del.textContent = rmvCheckBoxes[i].value;
+            //console.log("vlauaa" + del.textContent);
+            label.innerHTML = "<del>" + label.textContent + "</del>";
+            //label.appendChild(del);
         }
-      
-    }  
+        if (rmvCheckBoxes[i].value == "off") {
+            var label = document.querySelector("label");
+            console.log(rmvCheckBoxes[i].value);
+            label.setAttribute("for", rmvCheckBoxes[i].id);
+            label.innerHTML = "";
+            label.innerHTML = label.textContent;
+        }
+
+    }
 });
 
